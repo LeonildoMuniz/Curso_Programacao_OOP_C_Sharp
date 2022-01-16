0776121ed06ed473e859abc694efcdc34ed53a00
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2022-01-16 13:34:22 -0300

    Add files via upload

---
+++ Exercícios diversos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace excercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Qual é a cotação do dolar?: ");
+            double vlr = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar?: ");
+            double qtd = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valor = metodo_estatico.ConversorDeMoedas.Moeda(vlr, qtd);
+
+            Console.Write("Valor a ser pago em reais: R$ " + valor.ToString("F2",CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
