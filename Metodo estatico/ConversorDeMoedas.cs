d563e944acd016666f43d430157ca7b36e314fd9
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2022-01-16 13:37:22 -0300

    Add files via upload

---
+++ Metodo estatico/ConversorDeMoedas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodo_estatico
+{
+    internal class ConversorDeMoedas
+    {
+        double A, B;
+
+        public static double Moeda(double A, double B)
+        {
+            return A * B;
+        } 
+    }
+}
