9bb2d19cd14c780463ced65491e27201492397b3
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2022-01-18 21:29:33 -0300

    Add files via upload

---
+++ ex_vetor/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_vetor
+{
+    internal class Reserva
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public Reserva(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;        }
+    }
+}
